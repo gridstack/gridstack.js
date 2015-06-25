6ffdf307f825722720531bd2a1437ce4e9d552ba
Pavel Reznikov <pashka.reznikov@gmail.com>
2015-06-24 18:48:44 -0700

    file header version

--- src/gridstack.js
+++ src/gridstack.js
@@ -1,4 +1,4 @@
-//     gridstack.js 0.2.3-dev
+//     gridstack.js 0.2.4-dev
 //     http://troolee.github.io/gridstack.js/
 //     (c) 2014-2015 Pavel Reznikov
 //     gridstack.js may be freely distributed under the MIT license.
