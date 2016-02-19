504b0ad4ac0fb5d2c7e28011c8b4f00b23c3237c
Kevin Dietrich <hallo@kevin-dietrich.com>
2016-02-19 19:58:32 +0100

    Added coverage

--- karma.conf.js
+++ karma.conf.js
@@ -31,13 +31,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'src/gridstack.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
