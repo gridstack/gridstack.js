495918f9c7b66d3ed8eff9611d0421d985346d5d
Pavel Reznikov <pashka.reznikov@gmail.com>
2016-02-20 00:11:17 -0800

    build dist

--- dist/gridstack.js
+++ dist/gridstack.js
@@ -98,6 +98,20 @@
 
         _isAddNodeIntercepted: function(n) {
             return Utils.isIntercepted({x: this.x, y: this.y, width: this.node.width, height: this.node.height}, n);
+        },
+
+        parseHeight: function(val) {
+            var height = val;
+            var heightUnit = 'px';
+            if (height && _.isString(height)) {
+                var match = height.match(/^([0-9]*\.[0-9]+|[0-9]+)(px|em|rem|vh|vw)?$/);
+                if (!match) {
+                    throw new Error('Invalid height');
+                }
+                heightUnit = match[2];
+                height = parseFloat(match[1]);
+            }
+            return {height: height, unit: heightUnit};
         }
     };
 
@@ -514,8 +528,18 @@
                 appendTo: 'body'
             }),
             disableDrag: opts.disableDrag || false,
-            disableResize: opts.disableResize || false
+            disableResize: opts.disableResize || false,
+            rtl: 'auto'
         });
+
+        if (this.opts.rtl === 'auto') {
+            this.opts.rtl = this.container.css('direction') === 'rtl';
+        }
+
+        if (this.opts.rtl) {
+            this.container.addClass('grid-stack-rtl');
+        }
+
         this.opts.isNested = isNested;
 
         this.cellHeight(this.opts.cellHeight, true);
@@ -1125,26 +1149,12 @@
         });
     };
 
-    function parseHeight(val) {
-        var height = val;
-        var heightUnit = 'px';
-        if (height && _.isString(height)) {
-            var match = height.match(/^([0-9]*\.[0-9]+|[0-9]+)(px|em|rem|vh|vw)?$/);
-            if (!match) {
-                throw new Error('Invalid height');
-            }
-            heightUnit = match[2];
-            height = parseFloat(match[1]);
-        }
-        return {height: height, unit: heightUnit};
-    }
-
     GridStack.prototype.verticalMargin = function(val, noUpdate) {
         if (typeof val == 'undefined') {
             return this.opts.verticalMargin;
         }
 
-        var heightData = parseHeight(val);
+        var heightData = Utils.parseHeight(val);
 
         if (this.opts.verticalMarginUnit === heightData.unit && this.opts.height === heightData.height) {
             return ;
@@ -1165,7 +1175,7 @@
             var o = this.container.children('.' + this.opts.itemClass).first();
             return Math.ceil(o.outerHeight() / o.attr('data-gs-height'));
         }
-        var heightData = parseHeight(val);
+        var heightData = Utils.parseHeight(val);
 
         if (this.opts.cellHeightUnit === heightData.heightUnit && this.opts.height === heightData.height) {
             return ;
