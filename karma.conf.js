e5f2a42877c7579901db5fc368495d69a84618ed
Alain Dumesny <alain.dumesny@gmail.com>
2019-03-26 08:40:31 -0700

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
         // list of files / patterns to load in the browser
         files: [
-          'bower_components/jquery/dist/jquery.min.js',
-          'bower_components/jquery-ui/jquery-ui.min.js',
+          'node_modules/jquery/dist/jquery.min.js',
+          'node_modules/components-jqueryui/jquery-ui.min.js',
           'node_modules/core-js/client/shim.min.js',
           'src/gridstack.js',
           'src/gridstack.jQueryUI.js',
