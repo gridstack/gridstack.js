989d92ec8ec4bdea625133c66fe5752f916aee2e
Alain Dumesny <alain.dumesny@gmail.com>
2020-03-04 19:53:30 -0800

    Add polyfill for CustomEvent to fix #1186 (#1187)
    * Add polyfill for CustomEvent to fix #1186

--- src/gridstack-poly.js
+++ src/gridstack-poly.js
@@ -5,6 +5,22 @@
  * gridstack.js may be freely distributed under the MIT license.
 */
 
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+(function () {
+  if (typeof window.CustomEvent === "function") {
+    return false;
+  }
+
+  function CustomEvent (event, params) {
+    params = params || {bubbles: false, cancelable: false, detail: null};
+    var evt = document.createEvent('CustomEvent');
+    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+    return evt;
+  }
+
+  window.CustomEvent = CustomEvent;
+})();
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isNaN
 Number.isNaN = Number.isNaN || function isNaN(input) {
   return typeof input === 'number' && input !== input;
