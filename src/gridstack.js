a6e39840b6a08dbc499ccbc875dfefc6b0f349df
Andrei Radoi <andreiradoidev@gmail.com>
2020-03-16 15:06:16 +0200

    Replaced JSON method with jQuery.extends

--- src/gridstack.js
+++ src/gridstack.js
@@ -2112,7 +2112,7 @@
     var grids = [];
     $(selector).each(function(index, el) {
       if (!el.gridstack) {
-        el.gridstack = new GridStack(el, JSON.parse(JSON.stringify(opts)));
+        el.gridstack = new GridStack(el, Utils.clone(opts));
       }
       grids.push(el.gridstack);
     });
