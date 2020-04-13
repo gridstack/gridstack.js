5418a61191169652b73ee3b4e4b3c54e9b1a6629
Alain Dumesny <alaind831+github@gmail.com>
2020-04-12 23:16:00 -0700

    TS: doc/types cleanup * GridStackNode.grid is not hidden (was _grid) * doc updated to hide more private members

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: {
     'gridstack.all': './src/index.ts'
   },
-  mode: 'development', // production vs development
+  mode: 'production', // production vs development
   module: {
     rules: [
       {
