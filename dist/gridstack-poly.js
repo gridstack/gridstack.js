4019a02b619af9ff7938f95a2070293a65fc75a8
Joshua Dean <joshua.dean@ad.corp.internal.essium.co>
2020-05-13 11:48:17 -0400

    Dist

---
+++ dist/gridstack-poly.js
@@ -0,0 +1,121 @@
+/** gridstack.js 1.1.1-dev - IE and older browsers Polyfills for this library @preserve*/
+/**
+ * https://gridstackjs.com/
+ * (c) 2019-2020 Alain Dumesny
+ * gridstack.js may be freely distributed under the MIT license.
+*/
+
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+(function () {
+  if (typeof window.CustomEvent === "function") {
+    return false;
+  }
+
+  function CustomEvent (event, params) {
+    params = params || {bubbles: false, cancelable: false, detail: null};
+    var evt = document.createEvent('CustomEvent');
+    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+    return evt;
+  }
+
+  window.CustomEvent = CustomEvent;
+})();
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isNaN
+Number.isNaN = Number.isNaN || function isNaN(input) {
+  return typeof input === 'number' && input !== input;
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value: function (predicate) {
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return kValue.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return undefined.
+      return undefined;
+    },
+    configurable: true,
+    writable: true
+  });
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+if (!Array.prototype.findIndex) {
+  Object.defineProperty(Array.prototype, 'findIndex', {
+    value: function(predicate) {
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      var thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      var k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return k.
+        var kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return k;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return -1.
+      return -1;
+    },
+    configurable: true,
+    writable: true
+  });
+}
---
+++ dist/gridstack.jQueryUI.js
@@ -0,0 +1,90 @@
+/** gridstack.js 1.1.1-dev - JQuery UI Drag&Drop plugin @preserve */
+/**
+ * https://gridstackjs.com/
+ * (c) 2014-2020 Alain Dumesny, Dylan Weiss, Pavel Reznikov
+ * gridstack.js may be freely distributed under the MIT license.
+*/
+(function(factory) {
+  /* we compile this in so no need for required loading
+  if (typeof define === 'function' && define.amd) {
+    define(['jquery', 'gridstack', 'exports'], factory);
+  } else if (typeof exports !== 'undefined') {
+    try { jQuery = require('jquery'); } catch (e) {}
+    try { gridstack = require('gridstack'); } catch (e) {}
+    factory(jQuery, gridstack.GridStack, exports);
+  } else */{
+    factory(jQuery, GridStack, window);
+  }
+})(function($, GridStack, scope) {
+  /**
+  * @class JQueryUIGridStackDragDropPlugin
+  * jQuery UI implementation of drag'n'drop gridstack plugin.
+  */
+  function JQueryUIGridStackDragDropPlugin(grid) {
+    GridStack.DragDropPlugin.call(this, grid);
+  }
+
+  GridStack.DragDropPlugin.registerPlugin(JQueryUIGridStackDragDropPlugin);
+
+  JQueryUIGridStackDragDropPlugin.prototype = Object.create(GridStack.DragDropPlugin.prototype);
+  JQueryUIGridStackDragDropPlugin.prototype.constructor = JQueryUIGridStackDragDropPlugin;
+
+  JQueryUIGridStackDragDropPlugin.prototype.resizable = function(el, opts) {
+    el = $(el);
+    if (opts === 'disable' || opts === 'enable' || opts === 'destroy') {
+      el.resizable(opts);
+    } else if (opts === 'option') {
+      var key = arguments[2];
+      var value = arguments[3];
+      el.resizable(opts, key, value);
+    } else {
+      var handles = el.data('gs-resize-handles') ? el.data('gs-resize-handles') :
+        this.grid.opts.resizable.handles;
+      el.resizable($.extend({}, this.grid.opts.resizable, {
+        handles: handles
+      }, {
+        start: opts.start || function() {},
+        stop: opts.stop || function() {},
+        resize: opts.resize || function() {}
+      }));
+    }
+    return this;
+  };
+
+  JQueryUIGridStackDragDropPlugin.prototype.draggable = function(el, opts) {
+    el = $(el);
+    if (opts === 'disable' || opts === 'enable' || opts === 'destroy') {
+      el.draggable(opts);
+    } else {
+      el.draggable($.extend({}, this.grid.opts.draggable, {
+        containment: (this.grid.opts.isNested && !this.grid.opts.dragOut) ?
+          this.grid.$el.parent() :
+          (this.grid.opts.draggable.containment || null),
+        start: opts.start || function() {},
+        stop: opts.stop || function() {},
+        drag: opts.drag || function() {}
+      }));
+    }
+    return this;
+  };
+
+  JQueryUIGridStackDragDropPlugin.prototype.droppable = function(el, opts) {
+    el = $(el);
+    el.droppable(opts);
+    return this;
+  };
+
+  JQueryUIGridStackDragDropPlugin.prototype.isDroppable = function(el, opts) {
+    el = $(el);
+    return Boolean(el.data('droppable'));
+  };
+
+  JQueryUIGridStackDragDropPlugin.prototype.on = function(el, eventName, callback) {
+    $(el).on(eventName, callback);
+    return this;
+  };
+
+  scope.JQueryUIGridStackDragDropPlugin = JQueryUIGridStackDragDropPlugin;
+
+  return JQueryUIGridStackDragDropPlugin;
+});
