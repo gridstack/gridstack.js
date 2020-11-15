9961c9711b3c4b2a620f132d49410d0e08e8fa08
Alain Dumesny <alaind831+github@gmail.com>
2020-11-15 07:40:59 -0800

    h5: rebase to develop 2.2.0 branch

---
+++ src/index-jq.ts
@@ -0,0 +1,13 @@
+// index.jq.ts 2.2.0-dev - everything you need for a Grid that uses Jquery-ui drag&drop (original, full feature) @preserve
+
+// import './gridstack-poly.js';
+
+export * from './types';
+export * from './utils';
+export * from './gridstack-engine';
+export * from './gridstack-dd';
+export * from './gridstack';
+
+export * from './jq/gridstack-dd-jqueryui';
+
+// declare module 'gridstack'; for umd ?
