38c154b778adba9375a18bc173e41878d8cc132b
Alain Dumesny <alaind831+github@gmail.com>
2021-03-28 21:11:53 -0700

    4.0.3-dev

--- src/gridstack-engine.ts
+++ src/gridstack-engine.ts
@@ -1,5 +1,5 @@
 /**
- * gridstack-engine.ts 4.0.3
+ * gridstack-engine.ts 4.0.3-dev
  * Copyright (c) 2021 Alain Dumesny - see GridStack root license
  */
 
--- src/gridstack.ts
+++ src/gridstack.ts
@@ -1,5 +1,5 @@
 /*!
- * GridStack 4.0.3
+ * GridStack 4.0.3-dev
  * https://gridstackjs.com/
  *
  * Copyright (c) 2021 Alain Dumesny
