bac5902ecaa8246ccd235dd3bae81f24348193be
Alain Dumesny <alaind831+github@gmail.com>
2021-04-23 11:13:03 -0700

    v4.2.2

--- src/h5/dd-draggable.ts
+++ src/h5/dd-draggable.ts
@@ -1,5 +1,5 @@
 /**
- * dd-draggable.ts 4.2.1
+ * dd-draggable.ts 4.2.2
  * Copyright (c) 2021 Alain Dumesny - see GridStack root license
  */
 
--- src/h5/dd-droppable.ts
+++ src/h5/dd-droppable.ts
@@ -1,5 +1,5 @@
 /**
- * dd-droppable.ts 4.2.1
+ * dd-droppable.ts 4.2.2
  * Copyright (c) 2021 Alain Dumesny - see GridStack root license
  */
 
