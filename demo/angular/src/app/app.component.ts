12f85e0a8646b2abc9b1ff5c47c152e52819462e
Alain Dumesny <alaind831+github@gmail.com>
2023-01-08 08:27:18 -0800

    web v7.2.0

--- demo/angular/src/app/app.component.ts
+++ demo/angular/src/app/app.component.ts
@@ -11,18 +11,23 @@ let ids = 1;
 })
 export class AppComponent {
   // which sample to show
-  show = 3;
+  public show = 4;
 
   /** sample grid options and items to load... */
-  public gridOptions: GridStackOptions = {
-    margin: 5,
-    float: true,
-  }
   public items: GridStackWidget[] = [
     {x: 0, y: 0, minW: 2},
     {x: 1, y: 1},
     {x: 2, y: 2},
   ];
+  public gridOptions: GridStackOptions = {
+    margin: 5,
+    float: true,
+    minRow: 1,
+  }
+  public gridOptionsFull: GridStackOptions = {
+    ...this.gridOptions,
+    children: this.items,
+  }
 
   constructor() {
     // give them content and unique id to make sure we track them during changes below...
@@ -34,8 +39,9 @@ export class AppComponent {
 
   /** called whenever items change size/position/etc.. */
   public onChange(data: nodesCB) {
+    // TODO: update our TEMPLATE list to match ?
+    // NOTE: no need for dynamic as we can always use grid.save() to get latest layout, or grid.engine.nodes
     console.log('change ', data.nodes.length > 1 ? data.nodes : data.nodes[0]);
-    // TODO: update our list to match ?
   }
 
   public onResizeStop(data: elementCB) {
@@ -43,36 +49,53 @@ export class AppComponent {
   }
 
   /**
-   * CRUD TEST operations
+   * TEST dynamic grid operations - uses grid API directly (since we don't track structure that gets out of sync)
    */
   public add(gridComp: GridstackComponent) {
+    gridComp.grid?.addWidget({x:3, y:0, w:2, content:`item ${ids}`, id:String(ids++)});
+  }
+  public delete(gridComp: GridstackComponent) {
+    gridComp.grid?.removeWidget(gridComp.grid.engine.nodes[0]?.el!);
+  }
+  public modify(gridComp: GridstackComponent) {
+    gridComp.grid?.update(gridComp.grid.engine.nodes[0]?.el!, {w:3})
+  }
+  public newLayout(gridComp: GridstackComponent) {
+    gridComp.grid?.load([
+      {x:0, y:1, id:'1', minW:1, w:1}, // new size/constrain
+      {x:1, y:1, id:'2'},
+      // {x:2, y:1, id:'3'}, // delete item
+      {x:3, y:0, w:2, content:'new item'}, // new item
+    ]);
+  }
+
+  /**
+   * TEST TEMPLATE operations for ngFor case - NOT recommended unless you have no GS creating/re-parenting
+   */
+  public addNgFor() {
     // new array isn't required as Angular detects changes to content with trackBy:identify()
     // this.items = [...this.items, { x:3, y:0, w:3, content:`item ${ids}`, id:String(ids++) }];
-    this.items.push({x:3, y:0, w:3, content:`item ${ids}`, id:String(ids++)});
+    this.items.push({x:3, y:0, w:2, content:`item ${ids}`, id:String(ids++)});
   }
-
-  public delete(gridComp: GridstackComponent) {
+  public deleteNgFor() {
     this.items.pop();
   }
-
-  public modify(gridComp: GridstackComponent) {
+  public modifyNgFor(gridComp: GridstackComponent) {
     // this will not update the DOM nor trigger gridstackItems.changes for GS to auto-update, so set new option of the gridItem instead
     // this.items[0].w = 3;
     const gridItem = gridComp.gridstackItems?.get(0);
     if (gridItem) gridItem.options = {w:3};
   }
-
-  /** test updating existing and creating new one */
-  public newLayout(gridComp: GridstackComponent) {
+  public newLayoutNgFor() {
     this.items = [
       {x:0, y:1, id:'1', minW:1, w:1}, // new size/constrain
       {x:1, y:1, id:'2'},
       // {x:2, y:1, id:'3'}, // delete item
-      {x:3, y:0, w:3, content:'new item'}, // new item
+      {x:3, y:0, w:2, content:'new item'}, // new item
     ];
   }
 
-  // ngFor unique node id to have correct match between our items used and GS
+  // ngFor TEMPLATE unique node id to have correct match between our items used and GS
   public identify(index: number, w: GridStackWidget) {
     return w.id; // or use index if no id is set and you only modify at the end...
   }
