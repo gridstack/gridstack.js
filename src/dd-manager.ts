7c0c92d884629ca09db1eedb6858e05b6359af25
Alain Dumesny <alaind831+github@gmail.com>
2023-05-06 18:07:20 -0700

    v8.1.0-dev

--- src/dd-manager.ts
+++ src/dd-manager.ts
@@ -1,5 +1,5 @@
 /**
- * dd-manager.ts 8.1.0
+ * dd-manager.ts 8.1.0-dev
  * Copyright (c) 2021 Alain Dumesny - see GridStack root license
  */
 
--- src/dd-touch.ts
+++ src/dd-touch.ts
@@ -1,5 +1,5 @@
 /**
- * touch.ts 8.1.0
+ * touch.ts 8.1.0-dev
  * Copyright (c) 2021 Alain Dumesny - see GridStack root license
  */
 
