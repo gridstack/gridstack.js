8715700c1b80d50ba25de60948c161c0ec53aaa2
Alain Dumesny <alaind831+github@gmail.com>
2023-05-22 07:20:08 -0700

    rev 8.1.2

--- angular/projects/lib/src/lib/gridstack-item.component.ts
+++ angular/projects/lib/src/lib/gridstack-item.component.ts
@@ -1,5 +1,5 @@
 /**
- * gridstack-item.component.ts 8.1.1-dev
+ * gridstack-item.component.ts 8.1.2
  * Copyright (c) 2022 Alain Dumesny - see GridStack root license
  */
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 /**
- * utils.ts 8.1.1-dev
+ * utils.ts 8.1.2
  * Copyright (c) 2021 Alain Dumesny - see GridStack root license
  */
 
