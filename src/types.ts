1ae148170ad28afd5c91bd1e372d7d934fba3005
Alain Dumesny <alaind831+github@gmail.com>
2023-09-14 08:35:27 -0700

    tweak to resizeToContentCB - now takes useAttr

--- src/types.ts
+++ src/types.ts
@@ -86,7 +86,7 @@ export type AddRemoveFcn = (parent: HTMLElement, w: GridStackWidget, add: boolea
 /** optional function called during save() to let the caller add additional custom data to the GridStackWidget structure that will get returned */
 export type SaveFcn = (node: GridStackNode, w: GridStackWidget) => void;
 
-export type ResizeToContentFcn = (els: GridItemHTMLElement) => void;
+export type ResizeToContentFcn = (el: GridItemHTMLElement, useAttr?: boolean) => void;
 
 /**
  * Defines the options for a Grid
