d0da4da39f160e6fbf99d7a7f6f2ad80a59dcb3a
Alain Dumesny <alaind831+github@gmail.com>
2023-09-30 10:49:59 -0700

    v9.3.0-dev

--- src/gridstack.ts
+++ src/gridstack.ts
@@ -1,5 +1,5 @@
 /*!
- * GridStack 9.3.0
+ * GridStack 9.3.0-dev
  * https://gridstackjs.com/
  *
  * Copyright (c) 2021-2022 Alain Dumesny
@@ -1788,7 +1788,7 @@ export class GridStack {
     return this;
   }
 
-  static GDRev = '9.3.0';
+  static GDRev = '9.3.0-dev';
 
   /* ===========================================================================================
    * drag&drop methods that used to be stubbed out and implemented in dd-gridstack.ts
