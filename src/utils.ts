0692ced8c3bf28fd2c7d9be79e0fe003bb8fe345
Alain Dumesny <alaind831+github@gmail.com>
2023-10-22 20:57:10 -0700

    parseHeight() fix
    * fix #2497
    * make sure to check for height=''

--- src/utils.ts
+++ src/utils.ts
@@ -215,12 +215,12 @@ export class Utils {
   static parseHeight(val: numberOrString): HeightData {
     let h: number;
     let unit = 'px';
-    if (typeof val === 'string') {
+    if (val && typeof val === 'string') {
       if (val === 'auto') h = 0;
       else {
         let match = val.match(/^(-[0-9]+\.[0-9]+|[0-9]*\.[0-9]+|-[0-9]+|[0-9]+)(px|em|rem|vh|vw|%)?$/);
         if (!match) {
-          throw new Error('Invalid height');
+          throw new Error(`Invalid height val = ${val}`);
         }
         unit = match[2] || 'px';
         h = parseFloat(match[1]);
