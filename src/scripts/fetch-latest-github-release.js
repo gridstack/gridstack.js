3c0493da07f83fb3a8663e7df5898ac5249a88c4
Damien Schneider <74979845+damien-schneider@users.noreply.github.com>
2024-08-07 05:30:04 +0200

    Automate latest release on home page (#2749)
    * ⬆️ upgrade gridstack to v10.3.1
    
    * :sparkles: Create script to get latest release name
    
    * 🚚 rename to a more explicit file name
    
    * 🔍 Add default value

---
+++ src/scripts/fetch-latest-github-release.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const repoOwner = 'gridstack' // Replace with the GitHub username or organization
+  const repoName = 'gridstack.js' // Replace with the repository name
+
+  // GitHub API URL for the latest release
+  const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/releases/latest`
+
+  // Fetch the latest release data from the GitHub API
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok ${response.statusText}`)
+      }
+      return response.json()
+    })
+    .then((data) => {
+      // Update the content of the <span> element with id "release-version"
+      const releaseVersionElement = document.getElementById('release-version')
+      releaseVersionElement.textContent = data.name
+    })
+    .catch((error) => {
+      console.error('There was a problem with the fetch operation:', error)
+      const releaseVersionElement = document.getElementById('release-version')
+      releaseVersionElement.textContent = 'Error loading release'
+    })
+})
