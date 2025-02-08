5066145f7e6a1fc120d36e7f176ebaaa88e55599
CNine <cnine229@gmail.com>
2025-02-08 12:11:32 +0800

    Add the GridStackContainer and update docs

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,4 +1,5 @@
 import "./demo.css";
+import { Simple0 } from "./examples/000-simple";
 import { Simple } from "./examples/001-simple";
 import { Nested } from "./examples/002-nested";
 import { CustomHandle } from "./examples/003-custom-handle";
@@ -7,8 +8,11 @@ import { Advanced } from "./examples/009-advanced";
 export default function App() {
   return (
     <div>
-      <h2 id="simple">Simple</h2>
+      <h2 id="simple0">Simple</h2>
       <p>Render content by GridStackItem with id selector.</p>
+      <Simple0 />
+      <h2 id="simple">Simple With Toolbar</h2>
+      <p>With toolbar</p>
       <Simple />
       <h2 id="nested">Nested</h2>
       <p>Only use gridstack.js native subGridOpts.</p>
