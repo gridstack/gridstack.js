5321400b73f23b746581bb0b5e57f89fcfefa684
Alain Dumesny <alaind831+github@gmail.com>
2025-03-16 16:39:55 -0700

    Locked not working as expected
    * fix #2981

--- src/gridstack.ts
+++ src/gridstack.ts
@@ -2459,14 +2459,16 @@ export class GridStack {
     const noResize = node.noResize || this.opts.disableResize;
 
     // check for disabled grid first
-    const disable = this.opts.staticGrid || (noMove && noResize);
+    const disable = node.locked || this.opts.staticGrid || (noMove && noResize);
     if (force || disable) {
       if (node._initDD) {
         this._removeDD(el); // nukes everything instead of just disable, will add some styles back next
         delete node._initDD;
       }
-      if (disable) el.classList.add('ui-draggable-disabled', 'ui-resizable-disabled'); // add styles one might depend on #1435
-      if (!force) return this;
+      if (disable) {
+        el.classList.add('ui-draggable-disabled', 'ui-resizable-disabled'); // add styles one might depend on #1435
+        return this;
+      }
     }
 
     if (!node._initDD) {
