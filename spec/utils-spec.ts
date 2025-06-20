ce46498ee2164980ef6091094ff274824c2d3a70
Nick Heinbaugh <nheinbaugh@clickup.com>
2025-06-20 14:31:53 -0700

    extracted scroll speed logic for better testability and added tests

--- spec/utils-spec.ts
+++ spec/utils-spec.ts
@@ -116,6 +116,58 @@ describe('gridstack utils', function() {
     });
   });
 
+  describe('_getScrollAmount', () => {
+    const innerHeight = 800;
+    const elHeight = 600;
+
+    it('should not scroll if element is inside viewport', () => {
+      const rect = { top: 100, bottom: 700, left: 0, right: 0, width: 50, height: 50, toJSON: () => '' };
+      const scrollAmount = Utils._getScrollAmount(rect as DOMRect, innerHeight, elHeight, -10);
+      expect(scrollAmount).toBe(0);
+    });
+
+    describe('scrolling up', () => {
+      it('should scroll up', () => {
+        const rect = { top: -20, bottom: 580, left: 0, right: 0, width: 50, height: 50, toJSON: () => '' };
+        const scrollAmount = Utils._getScrollAmount(rect as DOMRect, innerHeight, elHeight, -30);
+        expect(scrollAmount).toBe(-20);
+      });
+      it('should scroll up to bring dragged element into view', () => {
+        const rect = { top: -20, bottom: 580, left: 0, right: 0, width: 50, height: 50, toJSON: () => '' };
+        const scrollAmount = Utils._getScrollAmount(rect as DOMRect, innerHeight, elHeight, -10);
+        expect(scrollAmount).toBe(-10);
+      });
+      it('should scroll up when dragged element is larger than viewport', () => {
+        const rect = { top: -20, bottom: 880, left: 0, right: 0, width: 50, height: 50, toJSON: () => '' };
+        const scrollAmount = Utils._getScrollAmount(rect as DOMRect, innerHeight, 900, -30);
+        expect(scrollAmount).toBe(-30);
+      });
+    });
+
+    describe('scrolling down', () => {
+      it('should not scroll down if element is inside viewport', () => {
+        const rect = { top: 100, bottom: 700, left: 0, right: 0, width: 50, height: 50, toJSON: () => '' };
+        const scrollAmount = Utils._getScrollAmount(rect as DOMRect, innerHeight, elHeight, 10);
+        expect(scrollAmount).toBe(0);
+      });
+      it('should scroll down', () => {
+        const rect = { top: 220, bottom: 820, left: 0, right: 0, width: 50, height: 50, toJSON: () => '' };
+        const scrollAmount = Utils._getScrollAmount(rect as DOMRect, innerHeight, elHeight, 10);
+        expect(scrollAmount).toBe(10);
+      });
+      it('should scroll down to bring dragged element into view', () => {
+        const rect = { top: 220, bottom: 820, left: 0, right: 0, width: 50, height: 50, toJSON: () => '' };
+        const scrollAmount = Utils._getScrollAmount(rect as DOMRect, innerHeight, elHeight, 30);
+        expect(scrollAmount).toBe(20);
+      });
+      it('should scroll down when dragged element is larger than viewport', () => {
+        const rect = { top: -100, bottom: 820, left: 0, right: 0, width: 50, height: 50, toJSON: () => '' };
+        const scrollAmount = Utils._getScrollAmount(rect as DOMRect, innerHeight, 920, 10);
+        expect(scrollAmount).toBe(10);
+      });
+    });
+  });
+
   describe('clone', () => {
     const a: any = {first: 1, second: 'text'};
     const b: any = {first: 1, second: {third: 3}};
