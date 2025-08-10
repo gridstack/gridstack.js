d31b75a2f598a31ebb42fc6e67414708ce60a5f4
Alain Dumesny <alain.dumesny@broadcom.com>
2025-08-09 23:26:31 -0700

    improved angular code doc

--- angular/projects/lib/src/lib/gridstack.module.ts
+++ angular/projects/lib/src/lib/gridstack.module.ts
@@ -8,7 +8,29 @@ import { NgModule } from "@angular/core";
 import { GridstackItemComponent } from "./gridstack-item.component";
 import { GridstackComponent } from "./gridstack.component";
 
-// @deprecated use GridstackComponent and GridstackItemComponent as standalone components
+/**
+ * @deprecated Use GridstackComponent and GridstackItemComponent as standalone components instead.
+ * 
+ * This NgModule is provided for backward compatibility but is no longer the recommended approach.
+ * Import components directly in your standalone components or use the new Angular module structure.
+ * 
+ * @example
+ * ```typescript
+ * // Preferred approach - standalone components
+ * @Component({
+ *   selector: 'my-app',
+ *   imports: [GridstackComponent, GridstackItemComponent],
+ *   template: '<gridstack></gridstack>'
+ * })
+ * export class AppComponent {}
+ * 
+ * // Legacy approach (deprecated)
+ * @NgModule({
+ *   imports: [GridstackModule]
+ * })
+ * export class AppModule {}
+ * ```
+ */
 @NgModule({
   imports: [
     GridstackItemComponent,
